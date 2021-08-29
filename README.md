1e673f1135025d9037593a6e9e1f1b964ec62feb
baroka <birotori@gmail.com>
2021-08-29 13:28:37 +0100

    rspec and task order by DESC

--- README.md
+++ README.md
@@ -9,3 +9,43 @@ Things you may want to cover:
 * System dependencies look gemfile
 * Database : postgrsql
 * Deployment instructions
+
+How to deploy on heroku
+Deployment on heroku follows some steps describe below.
+
+##Creation of the app on heroku
+Just do on the terminal these commands
+<code>
+  $ heroku login
+  $ heroku create
+</code>
+##Change the stack because we use ruby version 2.6.5
+<code>
+  $ heroku buildpacks:set heroku/ruby
+  $ heroku buildpacks:add --index 1 heroku/nodejs
+  $ heroku stack:set heroku-18
+</code>
+##Compile the app and push it on heroku
+<code>
+  $ rails assets:precompile RAILS_ENV=production
+  $ git add -A
+  $ git commit -m "message"
+  $ git push heroku master
+</code>
+##Migrate database and open app link
+<code>
+  $ heroku run rails db:migrate
+  $ heroku open
+</code>
+
+List of gems required
+<code>
+  gem 'rails', '~> 5.2.6'
+  gem 'pg', '>= 0.18', '< 2.0'
+  gem 'puma', '~> 3.11'
+  gem 'sass-rails', '~> 5.0'
+  gem 'uglifier', '>= 1.3.0'
+  gem 'coffee-rails', '~> 4.2'
+  gem 'turbolinks', '~> 5'
+  gem 'jbuilder', '~> 2.5'
+</code>
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -2,8 +2,8 @@
 require 'rails_helper'
 RSpec.describe 'Fonction de gestion des tâches', type: :system do
   before do
-    FactoryBot.create(:task, task_name: 'task1', description: 'task1_content', start:'2021-08-30', deadline:'2021-08-31')
-    FactoryBot.create(:second_task, task_name: 'task2', description: 'task2_content', start:'2021-08-30', deadline:'2021-08-31')
+    task = FactoryBot.create(:task, task_name: 'task1', description: 'task1_content', start:'2021-08-30', deadline:'2021-08-31')
+    second_task = FactoryBot.create(:second_task, task_name: 'task2', description: 'task2_content', start:'2021-08-30', deadline:'2021-08-31')
   end
   describe 'Nouvelle fonction de création' do
     context 'Lors de la création d\'une nouvelle tâche' do
@@ -16,13 +16,18 @@ RSpec.describe 'Fonction de gestion des tâches', type: :system do
   end
   describe 'Fonction d\'affichage de liste' do
     context "Lors de la transition vers l\'écran de liste" do
-      it 'La liste des tâches créées s\'affiche' do
+      it 'La liste des tâches créées s\'affiche en ordre decroissant' do
+        visit tasks_path
+        assert Task.all.order(created_at: :DESC)
       end
     end
   end
   describe 'Fonction d\'affichage détaillée' do
      context 'Lors de la transition vers un écran de détails de tâche' do
        it 'Le contenu de la tâche concernée s\'affiche' do
+         visit tasks_path
+         click_on 'task1'
+         expect(page).to have_content 'task1_content'
        end
      end
   end
